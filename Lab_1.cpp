9a7578a1035c25eb0c2e03e9546a63e1d9e6f88d
Varnika-06 <varnikastuff234@gmail.com>
2025-07-22 01:20:45 +0530

    Create Lab_1.cpp

---
+++ Lab_1.cpp
@@ -0,0 +1,17 @@
+//NAME: Varnika Maurya
+//PRN: 24070123160
+//EXP-1
+//Program-1
+#include <iostream>  
+using namespace std;
+int main() {
+    cout<<"Hello World"<<endl;
+    cout<<"First Program";
+    return 0;  
+}
+/*
+PS C:\Users\DELL\Desktop> g++ Lab_1.cpp -o L2
+PS C:\Users\DELL\Desktop> .\Lab_1.exe
+Hello World
+First Program
+*/
